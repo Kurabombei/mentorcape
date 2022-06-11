baecfc8a03e6a008a808685ad0ef36331e85bb84
Kurabombei <ivan.volos1@gmail.com>
2022-06-11 19:51:30 +0300

    Hot fixes

--- src/app/pages/home-page/our-mentors/our-mentors.component.ts
+++ src/app/pages/home-page/our-mentors/our-mentors.component.ts
@@ -10,7 +10,43 @@ import {UtilService} from "../../../services/util.service";
 	styleUrls: ['./our-mentors.component.scss']
 })
 export class OurMentorsComponent implements OnInit {
-	mentors: User[] = [];
+	mentors: User[] = [
+		{
+			uid: 'asd',
+			email: 'aasd',
+
+			isMentor: true,
+
+			displayName: 'John Travolta',
+			position: 'Actor',
+			location: 'The USA',
+			photoURL: 'https://akns-images.eonline.com/eol_images/Entire_Site/2015122/rs_634x1024-150222213607-634.John-Travolta-Academy-Awards.ms.022215.jpg?fit=around%7C634:1024&output-quality=90&crop=634:1024;center,top',
+			socialLink: 'https://calendly.com/volos-work/30min?month=2022-06'
+		},
+		{
+			uid: 'asd',
+			email: 'aasd',
+
+			isMentor: true,
+
+			displayName: 'John Doe',
+			position: 'Actor',
+			location: 'The USA',
+			photoURL: 'https://akns-images.eonline.com/eol_images/Entire_Site/2015122/rs_634x1024-150222213607-634.John-Travolta-Academy-Awards.ms.022215.jpg?fit=around%7C634:1024&output-quality=90&crop=634:1024;center,top',
+			socialLink: 'https://calendly.com/volos-work/30min?month=2022-06'
+		}, {
+			uid: 'asd',
+			email: 'aasd',
+
+			isMentor: true,
+
+			displayName: 'John Travolta',
+			position: 'Actor',
+			location: 'The USA',
+			photoURL: 'https://akns-images.eonline.com/eol_images/Entire_Site/2015122/rs_634x1024-150222213607-634.John-Travolta-Academy-Awards.ms.022215.jpg?fit=around%7C634:1024&output-quality=90&crop=634:1024;center,top',
+			socialLink: 'https://calendly.com/volos-work/30min?month=2022-06'
+		}
+	];
 
 	private sub: Subscription = new Subscription();
 
@@ -21,7 +57,6 @@ export class OurMentorsComponent implements OnInit {
 		this.sub = this.dbService
 			.getTopMentors()
 			.subscribe((mentors: Array<any>) => (this.mentors = mentors));
-
 	}
 
 	navigateToUrl(socialLink?: string) {
